0c9b25968ca19a2d15a529f9b53901998e54c136
Ahmed ARIF <ahmed.arif_ext@softathome.com>
2023-03-17 11:51:45 +0100

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,215 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <lxc/lxccontainer.h>
+/* Feel free to use this example code in any way
+   you see fit (Public Domain) */
+
+
+
+#include "httphandler.h"
+#include "lxc-container.h"
+#include "lxcqueue.h"
+
+#define SEND_RESPONSE(connection, response_body) \
+    do { \
+    struct MHD_Response *mhd_response = MHD_create_response_from_buffer(response_body.size(), (void*) response_body.c_str(), MHD_RESPMEM_MUST_COPY); \
+    int ret = MHD_queue_response(connection, MHD_HTTP_OK, mhd_response); \
+    MHD_destroy_response(mhd_response); \
+    return ret; \
+    } while (0)
+
+// Function to execute a shell command and return its output
+std::string exec(const char* cmd) {
+    std::string result = "";
+    char buffer[128];
+    FILE* pipe = popen(cmd, "r");
+    if (!pipe) throw std::runtime_error("popen() failed!");
+    try {
+        while (fgets(buffer, sizeof buffer, pipe) != NULL) {
+            result += buffer;
+        }
+    } catch (...) {
+        pclose(pipe);
+        throw;
+    }
+    pclose(pipe);
+    return result;
+}
+
+int supported_API_versions(struct MHD_Connection *connection
+                           , const std::unordered_map<std::string, std::string>& params
+                           , const std::string &request_body) {
+    // Return a response with the message "Hello, world!"
+    Json::Value root;
+    root["metadata"].append("/1.0");
+    root["status"] = "Success";
+    root["status_code"] = 200;
+    root["type"] = "sync";
+
+    std::cout << root.toStyledString() << std::endl;
+
+    SEND_RESPONSE(connection, root.toStyledString());
+}
+
+
+int handle_echo(struct MHD_Connection *connection
+                , const std::unordered_map<std::string, std::string>& params
+                , const std::string &request_body) {
+    //    for (const auto& kv : params) {
+    //        std::cout << kv.first << " = " << kv.second << std::endl;
+    //    }
+    //    struct MHD_Response *mhd_response = MHD_create_response_from_buffer(request_body.size(), (void*) request_body.c_str(), MHD_RESPMEM_MUST_COPY);
+    //    int ret = MHD_queue_response(connection, MHD_HTTP_OK, mhd_response);
+    //    MHD_destroy_response(mhd_response);
+    //    return ret;
+    std::cout << params.at("value1") << std::endl;
+    SEND_RESPONSE(connection, request_body);
+}
+int get_Server_environment(struct MHD_Connection *connection
+                           , const std::unordered_map<std::string, std::string>& params
+                           , const std::string &request_body){
+    // Create a Json::Value object
+    Json::Value root;
+
+    // Set the metadata field
+    root["metadata"]["api_extensions"].append("etag");
+    root["metadata"]["api_extensions"].append("patch");
+    root["metadata"]["api_extensions"].append("network");
+    root["metadata"]["api_extensions"].append("storage");
+    root["metadata"]["api_status"] = "stable";
+    root["metadata"]["api_version"] = "1.0";
+    root["metadata"]["auth"] = "untrusted";
+    root["metadata"]["auth_methods"].append("tls");
+    root["metadata"]["auth_methods"].append("candid");
+
+    // Get system information using Linux syscalls
+    struct utsname sys_info;
+    if (uname(&sys_info) == -1) {
+        std::cerr << "Failed to get system information" << std::endl;
+        return 1;
+    }
+    // Set the config field
+    root["metadata"]["config"]["core.https_address"] = ":8443";
+    root["metadata"]["config"]["core.trust_password"] = true;
+
+    // Set the environment field
+    root["metadata"]["environment"]["addresses"].append(":8443");
+    root["metadata"]["environment"]["architectures"].append(sys_info.machine);
+    root["metadata"]["environment"]["certificate"] = "X509 PEM certificate";
+    root["metadata"]["environment"]["certificate_fingerprint"] = "fd200419b271f1dc2a5591b693cc5774b7f234e1ff8c6b78ad703b6888fe2b69";
+    root["metadata"]["environment"]["driver"] = "lxc";
+    root["metadata"]["environment"]["driver_version"] = "4.0.7 | 5.2.0";
+    root["metadata"]["environment"]["firewall"] = "nftables";
+    root["metadata"]["environment"]["kernel"] = sys_info.sysname;
+    root["metadata"]["environment"]["kernel_architecture"] = sys_info.machine;
+    root["metadata"]["environment"]["kernel_features"]["netnsid_getifaddrs"] = "true";
+    root["metadata"]["environment"]["kernel_features"]["seccomp_listener"] = "true";
+    root["metadata"]["environment"]["kernel_version"] = sys_info.release;
+    root["metadata"]["environment"]["lxc_features"]["cgroup2"] = "true";
+    root["metadata"]["environment"]["lxc_features"]["devpts_fd"] = "true";
+    root["metadata"]["environment"]["lxc_features"]["pidfd"] = "true";
+    root["metadata"]["environment"]["os_name"] = "Ubuntu";
+    root["metadata"]["environment"]["os_version"] = "22.04";
+    root["metadata"]["environment"]["project"] = "default";
+    root["metadata"]["environment"]["server"] = "lxcd";
+    root["metadata"]["environment"]["server_clustered"] = false;
+    //    root["metadata"]["environment"]["server_event_mode"] = "full-mesh";
+    root["metadata"]["environment"]["server_name"] = sys_info.nodename;
+    root["metadata"]["environment"]["server_pid"] = getpid();
+    root["metadata"]["environment"]["server_version"] = "1.0";
+    root["metadata"]["environment"]["storage"] = "dir | overlay";
+    SEND_RESPONSE(connection, root.toStyledString());
+}
+
+int execenv_create(struct MHD_Connection *connection
+                    , const std::unordered_map<std::string, std::string>& params
+                    , const std::string &request_body){
+    std::cout << "execenv_create" << std::endl;
+    Json::Value rootrequest;
+    Json::Value rootreply;
+    rootreply["metadata"].append("/1.0");
+    rootreply["type"] = "sync";
+
+    Json::CharReaderBuilder builder;
+    Json::CharReader* reader = builder.newCharReader();
+    std::string errors;
+    bool parsingSuccessful = reader->parse(request_body.c_str(), request_body.c_str() + request_body.size(), &rootrequest, &errors);
+    delete reader;
+
+    if (!parsingSuccessful) {
+      std::cerr << "Failed to parse JSON: " << errors << std::endl;
+      rootreply["status"] = "Failed to parse JSON: " + errors;
+      rootreply["status_code"] = 500;
+      SEND_RESPONSE(connection, rootreply.toStyledString());
+    }
+    std::string name = rootrequest["name"].asString();
+    std::string type = rootrequest["type"].asString();
+    std::cout << "Name: " << name << std::endl;
+    std::cout << "type: " << type << std::endl;
+
+    LxcContainer container(name);
+    container.setAction(Method::ENABLE);
+    container.setTemplate(type);
+    container.run();
+    rootreply["status"] = "Success";
+    rootreply["status_code"] = 200;
+    SEND_RESPONSE(connection, rootreply.toStyledString());
+}
+int execenv_ls(struct MHD_Connection *connection
+                , const std::unordered_map<std::string, std::string>& params
+                , const std::string &request_body){
+    Json::Value root;
+
+    // Set the metadata field
+    root["metadata"]["api_extensions"].append("etag");
+    root["metadata"]["api_extensions"].append("patch");
+    SEND_RESPONSE(connection, root.toStyledString());
+}
+
+int execenv_rm(struct MHD_Connection *connection
+                , const std::unordered_map<std::string, std::string>& params
+                , const std::string &request_body){
+    Json::Value root;
+
+    // Set the metadata field
+    root["metadata"]["api_extensions"].append("etag");
+    root["metadata"]["api_extensions"].append("patch");
+    SEND_RESPONSE(connection, root.toStyledString());
+}
+int execenv_update(struct MHD_Connection *connection
+                    , const std::unordered_map<std::string, std::string>& params
+                    , const std::string &request_body){
+    Json::Value root;
+
+    // Set the metadata field
+    root["metadata"]["api_extensions"].append("etag");
+    root["metadata"]["api_extensions"].append("patch");
+    SEND_RESPONSE(connection, root.toStyledString());
+}
+
 
 int main() {
-    struct lxc_container *c;
-    const char *name = "my-container";
-    const char *config_path = NULL;
-    const char *template = "ubuntu";
-    const char *flags = NULL;
-    int bdev_type = 0;
-    const char *bdev_data = "size=40MB";
-    const char *lxcpath = NULL;
+    // Create a RestApiListener instance on port 8080
+    RestApiListener rest_api(8080);
 
-    /* Create a new container */
-    c = lxc_container_new(name, config_path);
-    if (!c) {
-        printf("Failed to create container\n");
-        exit(1);
-    }
+    // handle those rpcs in this link https://linuxcontainers.org/lxd/docs/latest/api/#/server/api_get
+    // Register handlers for some example endpoints
+    rest_api.register_handler("/", "GET", supported_API_versions);
+    rest_api.register_handler("/2.0/", "GET", get_Server_environment);
 
-    /* Set the container's rootfs size to 40MB */
-    if (lxc_container_set_config_item(c, "lxc.rootfs.backend", "btrfs") < 0 ||
-        lxc_container_set_config_item(c, "lxc.rootfs.options", bdev_data) < 0) {
-        printf("Failed to set rootfs size\n");
-        exit(1);
-    }
+    rest_api.register_handler("/2.0/execenv", "POST", execenv_create);
+    rest_api.register_handler("/2.0/execenv", "GET", execenv_ls);
+    rest_api.register_handler("/2.0/execenv", "DELETE", execenv_rm);
+    rest_api.register_handler("/2.0/execenv", "UPDATE", execenv_update);
 
-    /* Create the container from a template */
-    if (lxc_container_create(c, template, flags, NULL, bdev_type, lxcpath) < 0) {
-        printf("Failed to create container\n");
-        exit(1);
-    }
+    rest_api.register_handler("/echo/{value1}", "GET", handle_echo);
 
-    /* Start the container */
-    if (lxc_container_start(c, 0, NULL) < 0) {
-        printf("Failed to start container\n");
-        exit(1);
-    }
+    // Start the listener
+    rest_api.start();
 
-    /* Stop the container */
-    if (lxc_container_stop(c) < 0) {
-        printf("Failed to stop container\n");
-        exit(1);
-    }
+    // Wait for user input to stop the listener
+    std::cout << "Press Enter to stop the REST API..." << std::endl;
+    std::cin.get();
 
-    /* Destroy the container */
-    if (lxc_container_destroy(c) < 0) {
-        printf("Failed to destroy container\n");
-        exit(1);
-    }
+    // Stop the listener
+    rest_api.stop();
 
-    lxc_container_put(c);
     return 0;
 }
