b25326e55017dd0856679a36ce1da2a437d10434
Ahmed ARIF <ahmed.arif_ext@softathome.com>
2023-04-10 19:02:42 +0200

    WIP

--- du-manager/main.cpp
+++ du-manager/main.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
+#include <getopt.h>
 #include "utils/uuid.h"
-
 #include "deployment_unit_helper.h"
 
 void printUsage() {
@@ -8,10 +8,19 @@ void printUsage() {
     printf("Options:\n");
     printf("  --ls                           List installed DeploymentUnits\n");
     printf("  --install -e <container> -d <path> [-u <uuid>]   Install a DeploymentUnit\n");
-    printf("  --remove <uuid>                Remove a DeploymentUnit by UUID\n");
+    printf("  --remove -u <uuid>                Remove a DeploymentUnit by UUID\n");
 }
 
 int main(int argc, char* argv[]) {
+
+    int c;
+    int option_index = 0;
+    static struct option long_options[] = {
+        {"ls", no_argument, 0, 'l'},
+        {"install", required_argument, 0, 'i'},
+        {"remove", required_argument, 0, 'r'},
+        {0, 0, 0, 0}
+    };
 
     if (argc < 2) {
         printUsage();
@@ -21,58 +30,67 @@ int main(int argc, char* argv[]) {
     char* action = argv[1];
     DeploymentUnitHelper helper;
 
-    if (strcmp(action, "--ls") == 0) {
-        helper.listDeploymentUnits();
-    } else if (strcmp(action, "--install") == 0) {
-        char* container = NULL;
-        char* path = NULL;
-        lxcd::string uuidStr;
-        uuid_t _uuid;
+    while ((c = getopt_long(argc, argv, "e:d:u:", long_options, &option_index)) != -1) {
+        switch (c) {
+            case 'l':{
+                helper.listDeploymentUnits();
+                return 0;
+                }
+            case 'i':{
+                char* container = NULL;
+                char* path = NULL;
+                lxcd::string uuidStr;
 
-        for (int i = 2; i < argc; i++) {
-            if (strcmp(argv[i], "-e") == 0 && i + 1 < argc) {
-                container = argv[++i];
-            } else if (strcmp(argv[i], "-d") == 0 && i + 1 < argc) {
-                path = argv[++i];
-            } else if (strcmp(argv[i], "-u") == 0 && i + 1 < argc) {
-                uuidStr = argv[++i];
-            } else {
-                printf("Unknown or incomplete option: %s\n", argv[i]);
+                for (int i = optind - 1; i < argc; i++) {
+                    if (strcmp(argv[i], "-e") == 0 && i + 1 < argc) {
+                        container = argv[++i];
+                    } else if (strcmp(argv[i], "-d") == 0 && i + 1 < argc) {
+                        path = argv[++i];
+                    } else if (strcmp(argv[i], "-u") == 0 && i + 1 < argc) {
+                        uuidStr = argv[++i];
+                    } else {
+                        printf("Unknown or incomplete option: %s\n", argv[i]);
+                        printUsage();
+                        return 1;
+                    }
+                }
+
+                if (container == NULL || path == NULL) {
+                    printf("Error: Both -e <container> and -d <path> options are required for --install.\n");
+                    printUsage();
+                    return 1;
+                }
+                if(uuidStr.empty())
+                    uuidStr = lxcd::UUIDGenerator::generate();
+
+                if (!helper.addDeploymentUnit(container, path, uuidStr)){
+                    printf("Error: Failed to install the DeploymentUnit.\n");
+                    return 1;
+                }
+                return 0;
+                }
+            case 'r':{
+                if (argc != optind + 1) {
+                    printf("Error: UUID is required for --remove.\n");
+                    printUsage();
+                    return 1;
+                }
+
+                char* uuidToRemove = argv[optind];
+                if (helper.removeDeploymentUnit(uuidToRemove)) {
+                    printf("Successfully removed DeploymentUnit with UUID: %s\n", uuidToRemove);
+                } else {
+                    printf("Error: Failed to remove the DeploymentUnit with UUID: %s\n", uuidToRemove);
+                    return 1;
+                }
+                return 0;
+                }
+            default:{
+                printf("Unknown action: %s\n", action);
                 printUsage();
                 return 1;
             }
         }
-
-        if (container == NULL || path == NULL) {
-            printf("Error: Both -e <container> and -d <path> options are required for --install.\n");
-            printUsage();
-            return 1;
-        }
-
-        uuidStr = lxcd::UUIDGenerator::generate();
-
-        if (!helper.addDeploymentUnit(container, path, uuidStr)){
-            printf("Error: Failed to install the DeploymentUnit.\n");
-            return 1;
-        }
-    } else if (strcmp(action, "--remove") == 0) {
-        if (argc != 3) {
-            printf("Error: UUID is required for --remove.\n");
-            printUsage();
-            return 1;
-        }
-
-        char* uuidToRemove = argv[2];
-        if (helper.removeDeploymentUnit(uuidToRemove)) {
-            printf("Successfully removed DeploymentUnit with UUID: %s\n", uuidToRemove);
-        } else {
-            printf("Error: Failed to remove the DeploymentUnit with UUID: %s\n", uuidToRemove);
-            return 1;
-        }
-    } else {
-        printf("Unknown action: %s\n", action);
-        printUsage();
-        return 1;
     }
 
     return 0;
